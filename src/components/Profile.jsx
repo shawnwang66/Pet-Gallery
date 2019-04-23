323fefe4d7f8c8bc25756f2c5c6f910e6aefef57
lingxiao <me@raymondlx.com>
2019-04-22 23:25:20 -0500

    skeleton for profile header

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -2,30 +2,93 @@ import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import styles from './Profile.module.scss'
 import axios from "axios";
+import {Image} from 'semantic-ui-react'
 
 class Profile extends Component{
 
     constructor(){
         super();
         this.state = {
-            profile_img : "",
-            basURL : "http://localhost:4000/api/"
+            name: "",
+            location: "",
+            ratings: 0,
+            image: ""
         }
 
+        this.login = this.login.bind(this);
         this.getUserInfo = this.getUserInfo.bind(this);
+
     }
 
-    getUserInfo(){
-        axios.get()
+    login(baseURL){
+        // login
+        axios.post(baseURL + 'login',{
+            username : "raymondlx",
+            password : "123456"
+        })
+            .then((response)=>{
+                // console.log(response);
+                window.localStorage.setItem('token',response.data.token);
+            })
     }
 
+    getUserInfo(baseURL, token){
+        // get user info
+
+        const config ={
+            headers: {'Authorization': "bearer " + token}
+        }
+
+        axios.get(baseURL + 'user', config)
+            .then((response)=>{
+                const resData = response.data.data;
+                console.log(resData)
+
+                // calculate ratings
+                const ratings = resData.ratings;
+                let sum = 0;
+                ratings.forEach((rating)=>{
+                    sum += rating;
+                });
+                sum /= ratings.length;
+
+                // update state
+                this.setState({name:resData.name, location: resData.location, ratings: sum, image: resData.imageURL})
+            })
+    }
+
+    async componentWillMount() {
+        const baseURL = 'http://localhost:4000/api/';
+        let token;
+        // login adn get token
+        await this.login(baseURL);
+        token = window.localStorage.getItem('token');
+
+        // get user info
+        await this.getUserInfo(baseURL, token)
+
+
+
+
+    }
 
 
     render(){
 
         return(
-            <div className={styles.testCase}>
-                <h1>Init</h1>
+            <div>
+                <div>
+                    <div>
+                        <h1>{this.state.name}</h1>
+                        <h2>{this.state.location}</h2>
+                        <p>{this.state.ratings}</p>
+                    </div>
+
+                    <div>
+                        <img src={this.state.image}></img>
+                    </div>
+
+                </div>
             </div>
         );
     }
