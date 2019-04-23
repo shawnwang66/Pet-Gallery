e8394c6a1e658a72acaa330e36b80bd578c9f721
lingxiao <me@raymondlx.com>
2019-04-22 22:15:14 -0500

    add axios

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,12 +1,24 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import styles from './Profile.module.scss'
+import axios from "axios";
 
 class Profile extends Component{
 
     constructor(){
         super();
+        this.state = {
+            profile_img : "",
+            basURL : "http://localhost:4000/api/"
+        }
+
+        this.getUserInfo = this.getUserInfo.bind(this);
     }
+
+    getUserInfo(){
+        axios.get()
+    }
+
 
 
     render(){
