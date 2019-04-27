1e286c4e8e4e30fa19c3bef043ea6b8834c3fdd2
dongaiiqng <dongaiiqng@gmail.com>
2019-04-27 17:07:54 -0500

    Added register

--- src/App.js
+++ src/App.js
@@ -4,9 +4,11 @@ import {BrowserRouter as Router, Route, Switch,HashRouter} from 'react-router-do
 import './App.scss';
 import Profile from './components/Profile/Profile';
 import Login from './components/Login/Login';
+import Register from './components/Login/Register';
 import NavBar from './components/NavBar/NavBar';
 import MainView from './components/MainView/MainView';
 import { library } from '@fortawesome/fontawesome-svg-core';
+import { faDove, faDog, faCat } from '@fortawesome/free-solid-svg-icons';
 
 
 class App extends Component {
@@ -17,10 +19,14 @@ class App extends Component {
               <Route exact path="/" component={MainView}/>
               <Route exact path="/profile" component={Profile}/>
               <Route exact path="/login" component={Login}/>
+              <Route exact path="/register" component={Register}/>
           </Switch>
         </HashRouter>
     )
   }
 }
+
+const fonts = [ faDove, faCat, faDog ];
+library.add(fonts);
 
 export default App;
