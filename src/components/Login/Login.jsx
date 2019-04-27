3d0c68d698d03b1bb56ecabc50f991fac2216774
dongaiiqng <dongaiiqng@gmail.com>
2019-04-27 02:19:27 -0500

    Finished login functionality

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -2,51 +2,34 @@ import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import './Login.style.scss'
 import axios from "axios/index";
+import { Redirect } from 'react-router-dom';
 import Button from '@material-ui/core/Button/index';
 import NavBar from '../NavBar/NavBar'
 
 const API_URL = "http://pet-gallery.herokuapp.com/api/";
-const APP_NAME = "WELCOME BACK";
+const APP_NAME = "Pet Gallery";
 
 class Login extends Component {
 
     constructor(){
         super();
         this.state = {
-            disableSubmit: true
+            disableSubmit: true,
+            errorHappened: false,
+            redirect: false
         };
 
         this.userName = "";
         this.password = "";
-        this.login = this.login.bind(this);
         this.onInputChanged = this.onInputChanged.bind(this);
-    }
-
-    login(baseURL){
-        // login
-        axios.post(baseURL + 'login',{
-            username : "raymondlx",
-            password : "123456"
-        })
-            .then((response)=>{
-                // console.log(response);
-                window.localStorage.setItem('token',response.data.token);
-            })
-    }
-
-    getUserInfo(baseURL, token){
-        // get user info
-        const config ={
-            headers: {'Authorization': "bearer " + token}
-        }
-
-        axios.get(baseURL + 'user', config)
-            .then((response)=>{
-
-            })
+        this.submitLoginInfo = this.submitLoginInfo.bind(this);
     }
 
     onInputChanged(e) {
+        this.setState({
+            errorHappened: false
+        });
+
         const inputName = e.target.name;
         if (inputName === 'username')
             this.userName = e.target.value;
@@ -63,30 +46,41 @@ class Login extends Component {
             });
     }
 
-    async componentWillMount() {
-        window.localStorage.setItem('baseURL', API_URL);
-
-        let token;
-        // login adn get token
-        await this.login(API_URL);
-        token = window.localStorage.getItem('token');
-
-        // get user info
-        await this.getUserInfo(API_URL, token)
+    submitLoginInfo() {
+        // "raymondlx" "123456"
+        axios.post(API_URL + 'login',{
+            username : this.userName,
+            password : this.password
+        })
+        .then((res) => {
+            const token = res.data.token;
+            window.localStorage.setItem('token', token);
+            this.setState({
+                redirect: true
+            });
+        })
+        .catch((err) => {
+            console.log(`Error during login: ${err}`);
+            this.setState({
+                errorHappened: true
+            });
+        });
     }
 
     render() {
         return(
             <div className='cp-root'>
+                {this.state.redirect ? <Redirect to='/'/> : []}
                 <div className='wrapper form'>
                     <p className='app-title'>{APP_NAME}</p>
                     <div className='input_wrapper'>
                         <input id="usr_box" placeholder="Username" name="username" type="text" onChange={this.onInputChanged}></input>
                         <input id="pwd_box" placeholder="Password" name="pwd" type="password" onChange={this.onInputChanged}></input>
                     </div>
-                    <button className='submit-bt' disabled={this.state.disableSubmit}>
+                    <button className='submit-bt' disabled={this.state.disableSubmit} onClick={this.submitLoginInfo}>
                         <p className='bt-text'>Submit</p>
                     </button>
+                    {this.state.errorHappened ? <p className='error-text'>Invalid username or password :(</p> : []}
                 </div>
                 <div className='wrapper register'>
                     <p className="app-text">Do not have an account?</p>
